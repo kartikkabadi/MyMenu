9ff3a30e534ad6da09f0355fb4e0db4b5b2cd4ba
Kartik <133712749+kartikkabadi@users.noreply.github.com>
2026-07-16 23:47:15 +0530

    B1: Expose profile store within the package

--- EngineContracts/Sources/MyMonitorEngineContracts/ProfileStoreProtocol.swift
+++ EngineContracts/Sources/MyMonitorEngineContracts/ProfileStoreProtocol.swift
@@ -1,5 +1,7 @@
-protocol DisplayProfileStoring: Sendable {
+package protocol DisplayProfileStoring: Sendable {
   func loadSnapshot() async throws -> DisplayProfileSnapshot
   func saveSnapshot(_ snapshot: DisplayProfileSnapshot) async throws
-  func migrateLegacyIfNeeded(topology: DisplayTopologySnapshot) async throws -> MigrationReport
+  func migrateLegacyIfNeeded(
+    topology: DisplayTopologySnapshot
+  ) async throws -> MigrationReport
 }
