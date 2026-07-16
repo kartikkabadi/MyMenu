25e27caa68d4bcdd79533fef377df499265c498d
Kartik <133712749+kartikkabadi@users.noreply.github.com>
2026-07-16 23:44:47 +0530

    B1: Export control and transport contracts

--- EngineContracts/Sources/MyMonitorEngineContracts/ControlContracts.swift
+++ EngineContracts/Sources/MyMonitorEngineContracts/ControlContracts.swift
@@ -1,18 +1,28 @@
 import Foundation
 
-struct ControlReading: Equatable, Codable, Sendable {
-  let observed: ObservedBrightness
-  let method: DisplayControlMethod
-  let resourceID: PhysicalControlResourceID
+public struct ControlReading: Equatable, Codable, Sendable {
+  public let observed: ObservedBrightness
+  public let method: DisplayControlMethod
+  public let resourceID: PhysicalControlResourceID
+
+  public init(
+    observed: ObservedBrightness,
+    method: DisplayControlMethod,
+    resourceID: PhysicalControlResourceID
+  ) {
+    self.observed = observed
+    self.method = method
+    self.resourceID = resourceID
+  }
 }
 
-struct ControlWriteReceipt: Equatable, Codable, Sendable {
-  let operationID: EngineOperationID
-  let desired: NormalizedBrightness
-  let outcome: ControlWriteOutcome
-  let completedAt: EngineInstant
+public struct ControlWriteReceipt: Equatable, Codable, Sendable {
+  public let operationID: EngineOperationID
+  public let desired: NormalizedBrightness
+  public let outcome: ControlWriteOutcome
+  public let completedAt: EngineInstant
 
-  init(
+  public init(
     operationID: EngineOperationID,
     desired: NormalizedBrightness,
     outcome: ControlWriteOutcome,
@@ -25,7 +35,7 @@ struct ControlWriteReceipt: Equatable, Codable, Sendable {
   }
 }
 
-protocol DisplayControl: AnyObject, Sendable {
+public protocol DisplayControl: AnyObject, Sendable {
   var method: DisplayControlMethod { get }
   var domain: BrightnessControlDomain { get }
   var resourceID: PhysicalControlResourceID { get }
@@ -41,67 +51,97 @@ protocol DisplayControl: AnyObject, Sendable {
   func teardown() async
 }
 
-enum AppleNativeCapability: Equatable, Codable, Sendable {
+public enum AppleNativeCapability: Equatable, Codable, Sendable {
   case unavailable(ControlFailure?)
   case readable
   case writeUnvalidated
   case writeValidated
 }
 
-protocol AppleNativeTransport: Sendable {
+public protocol AppleNativeTransport: Sendable {
   func capability(for display: RuntimeDisplayID) async -> AppleNativeCapability
   func readBrightness(for display: RuntimeDisplayID) async throws -> NormalizedBrightness
   func writeBrightness(_ value: NormalizedBrightness, for display: RuntimeDisplayID) async throws
 }
 
-struct DDCValue: Equatable, Codable, Sendable {
-  let current: UInt16
-  let maximum: UInt16
+public struct DDCValue: Equatable, Codable, Sendable {
+  public let current: UInt16
+  public let maximum: UInt16
+
+  public init(current: UInt16, maximum: UInt16) {
+    self.current = current
+    self.maximum = maximum
+  }
 }
 
-struct DDCServiceCandidate: Equatable, Codable, Sendable {
-  let serviceID: DDCServiceID
-  let runtimeDisplayID: RuntimeDisplayID?
-  let resourceID: PhysicalControlResourceID
-  let confidence: CapabilityConfidence
+public struct DDCServiceCandidate: Equatable, Codable, Sendable {
+  public let serviceID: DDCServiceID
+  public let runtimeDisplayID: RuntimeDisplayID?
+  public let resourceID: PhysicalControlResourceID
+  public let confidence: CapabilityConfidence
+
+  public init(
+    serviceID: DDCServiceID,
+    runtimeDisplayID: RuntimeDisplayID?,
+    resourceID: PhysicalControlResourceID,
+    confidence: CapabilityConfidence
+  ) {
+    self.serviceID = serviceID
+    self.runtimeDisplayID = runtimeDisplayID
+    self.resourceID = resourceID
+    self.confidence = confidence
+  }
 }
 
-protocol IOAVTransport: Sendable {
-  func discoverServices(for snapshot: DisplayTopologySnapshot) async throws -> [DDCServiceCandidate]
+public protocol IOAVTransport: Sendable {
+  func discoverServices(
+    for snapshot: DisplayTopologySnapshot
+  ) async throws -> [DDCServiceCandidate]
   func readVCP(_ code: UInt8, service: DDCServiceID) async throws -> DDCValue
   func writeVCP(_ code: UInt8, value: UInt16, service: DDCServiceID) async throws
   func invalidate(service: DDCServiceID) async
 }
 
-struct GammaTable: Equatable, Codable, Sendable {
-  let red: [Float]
-  let green: [Float]
-  let blue: [Float]
+public struct GammaTable: Equatable, Codable, Sendable {
+  public let red: [Float]
+  public let green: [Float]
+  public let blue: [Float]
 
-  init(red: [Float], green: [Float], blue: [Float]) {
+  public init(red: [Float], green: [Float], blue: [Float]) {
     precondition(red.count == green.count && green.count == blue.count)
     self.red = red
     self.green = green
     self.blue = blue
   }
 
-  var sampleCount: Int {
+  public var sampleCount: Int {
     red.count
   }
 }
 
-protocol GammaTransport: Sendable {
+public protocol GammaTransport: Sendable {
   func captureBaseline(for display: RuntimeDisplayID) async throws -> GammaTable
-  func apply(_ table: GammaTable, to display: RuntimeDisplayID, owner: OwnerToken) async throws
+  func apply(
+    _ table: GammaTable,
+    to display: RuntimeDisplayID,
+    owner: ControlOwnerID
+  ) async throws
   func restoreBaseline(
     _ table: GammaTable,
     to display: RuntimeDisplayID,
-    owner: OwnerToken
+    owner: ControlOwnerID
   ) async
 }
 
-protocol ShadeTransport: Sendable {
-  func installNeutralShade(for display: RuntimeDisplayID, owner: OwnerToken) async throws
-  func setBrightness(_ value: NormalizedBrightness, for display: RuntimeDisplayID, owner: OwnerToken) async throws
-  func removeShade(for display: RuntimeDisplayID, owner: OwnerToken) async
+public protocol ShadeTransport: Sendable {
+  func installNeutralShade(
+    for display: RuntimeDisplayID,
+    owner: ControlOwnerID
+  ) async throws
+  func setBrightness(
+    _ value: NormalizedBrightness,
+    for display: RuntimeDisplayID,
+    owner: ControlOwnerID
+  ) async throws
+  func removeShade(for display: RuntimeDisplayID, owner: ControlOwnerID) async
 }
