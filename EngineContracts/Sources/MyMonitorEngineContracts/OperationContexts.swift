7f9d41ac1fe689c067165ebfc0a53de4ace7cd5a
Kartik <133712749+kartikkabadi@users.noreply.github.com>
2026-07-16 23:47:40 +0530

    B1: Export operation context contracts

--- EngineContracts/Sources/MyMonitorEngineContracts/OperationContexts.swift
+++ EngineContracts/Sources/MyMonitorEngineContracts/OperationContexts.swift
@@ -1,19 +1,49 @@
-import Foundation
+public struct BrightnessOperationContext: Equatable, Codable, Sendable {
+  public let operationID: EngineOperationID
+  public let engineGeneration: EngineGeneration
+  public let sessionGeneration: SessionGeneration
+  public let resourceID: PhysicalControlResourceID
+  public let reason: ReconfigurationReason
+  public let adjustmentKind: BrightnessAdjustmentKind
+  public let startedAt: EngineInstant
 
-struct BrightnessOperationContext: Equatable, Codable, Sendable {
-  let operationID: EngineOperationID
-  let engineGeneration: EngineGeneration
-  let sessionGeneration: SessionGeneration
-  let resourceID: PhysicalControlResourceID
-  let reason: ReconfigurationReason
-  let adjustmentKind: BrightnessAdjustmentKind
-  let startedAt: EngineInstant
+  public init(
+    operationID: EngineOperationID,
+    engineGeneration: EngineGeneration,
+    sessionGeneration: SessionGeneration,
+    resourceID: PhysicalControlResourceID,
+    reason: ReconfigurationReason,
+    adjustmentKind: BrightnessAdjustmentKind,
+    startedAt: EngineInstant
+  ) {
+    self.operationID = operationID
+    self.engineGeneration = engineGeneration
+    self.sessionGeneration = sessionGeneration
+    self.resourceID = resourceID
+    self.reason = reason
+    self.adjustmentKind = adjustmentKind
+    self.startedAt = startedAt
+  }
 }
 
-struct ResumeContext: Equatable, Codable, Sendable {
-  let reason: ReconfigurationReason
-  let engineGeneration: EngineGeneration
-  let sessionGeneration: SessionGeneration
-  let topologySignature: TopologySignature
-  let continuityEpoch: ContinuityEpoch
+public struct ResumeContext: Equatable, Codable, Sendable {
+  public let reason: ReconfigurationReason
+  public let engineGeneration: EngineGeneration
+  public let sessionGeneration: SessionGeneration
+  public let topologySignature: TopologySignature
+  public let continuityEpoch: ContinuityEpoch
+
+  public init(
+    reason: ReconfigurationReason,
+    engineGeneration: EngineGeneration,
+    sessionGeneration: SessionGeneration,
+    topologySignature: TopologySignature,
+    continuityEpoch: ContinuityEpoch
+  ) {
+    self.reason = reason
+    self.engineGeneration = engineGeneration
+    self.sessionGeneration = sessionGeneration
+    self.topologySignature = topologySignature
+    self.continuityEpoch = continuityEpoch
+  }
 }
